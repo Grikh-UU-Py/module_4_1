d1556e848a29e8e4cb3cf9ea7e28edf37f95b320
Grikh-UU-Py <5186415@mail.ru>
2024-10-15 02:19:38 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,16 @@
+# Задача "А как делить?":
+
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+# result1 = fake_divide(69, 3)
+# result2 = fake_divide(3, 0)
+# result3 = true_divide(49, 7)
+# result4 = true_divide(15, 0)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
